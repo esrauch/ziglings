11fc7ff2c57e85a9efff9dc18ebf2cd8b2c1b388
Em Rauch <esrauch@gmail.com>
2023-09-06 01:25:56 +0000

    Change setup.sh to download zig 11

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-BUILD=zig-linux-x86_64-0.12.0-dev.280+64d03faae 
+BUILD=zig-linux-x86_64-0.11.0
 
 pushd /tmp
 curl https://ziglang.org/builds/$BUILD.tar.xz -o /tmp/zig.tar.xz
 tar -xf /tmp/zig.tar.xz
 
 popd
-mv /tmp/$BUILD/ $PATH/.zig
+mv /tmp/$BUILD/ $PWD/.zig
 echo "export PATH=$PATH:$PWD/.zig" > ~/.bashrc
 
