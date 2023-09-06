88ebe83dd214b441adf59fe119d46632c08c8d05
Em Rauch <esrauch@gmail.com>
2023-09-06 01:19:07 +0000

    Fix setup.sh not using tmp properly

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 BUILD=zig-linux-x86_64-0.12.0-dev.280+64d03faae 
 
-cd /workspaces/ziglings/
+pushd /tmp
 curl https://ziglang.org/builds/$BUILD.tar.xz -o /tmp/zig.tar.xz
 tar -xf /tmp/zig.tar.xz
-mv /tmp/$BUILD/ .zig
+
+popd
+mv /tmp/$BUILD/ $PATH/.zig
 echo "export PATH=$PATH:$PWD/.zig" > ~/.bashrc
 
